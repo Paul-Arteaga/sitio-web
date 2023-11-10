58a7751a874baca2a1c6d62e9b734d411e8090c4
ernesto arteaga <paul>
2023-11-09 22:41:40 -0400

    Updates

--- index.js
+++ index.js
@@ -1,50 +1,3 @@
-/* document.addEventListener('DOMContentLoaded', function() {
-    console.log("Página cargada");
-
-    // Verificar si hay credenciales almacenadas
-    var credenciales = JSON.parse(localStorage.getItem("credenciales"));
-    
-    if (credenciales) {
-        // Llenar los campos de usuario y contraseña
-        document.querySelector('input[type="email"]').value = credenciales.correo;
-        document.querySelector('input[type="password"]').value = credenciales.contrasena;
-    }
-
-    // Asociar la función de inicio de sesión al botón
-    document.querySelector('button').addEventListener('click', function() {
-        console.log("Botón clickeado");
-
-        // Obtener los valores de los campos de usuario y contraseña
-        var usuario = document.querySelector('input[type="email"]').value;
-        var contrasena = document.querySelector('input[type="password"]').value;
-
-        // Verificar si las credenciales son válidas
-        if (verificarCredenciales(usuario, contrasena)) {
-            // Redirigir a la página de login.html
-            window.location.href = "login.html";
-            
-            // Mensaje de éxito (opcional)
-            alert("Inicio de sesión exitoso. Redirigiendo a login.html");
-        } else {
-            // Mensaje de error
-            alert("Credenciales incorrectas. Verifica tu usuario y contraseña.");
-        }
-    });
-
-    function verificarCredenciales(usuario, contrasena) {
-        // Obtener la lista de usuarios registrados
-        var usuariosRegistrados = JSON.parse(localStorage.getItem("usuarios")) || [];
-
-        // Verificar si las credenciales coinciden con algún usuario registrado
-        return usuariosRegistrados.some(function(u) {
-            return u.correo === usuario && u.contrasena === contrasena;
-        });
-    }
-});
- */
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     // Verificar si hay credenciales almacenadas
     var credenciales = JSON.parse(localStorage.getItem("credenciales"));
@@ -55,8 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('input[type="password"]').value = credenciales.contrasena;
     }
 
-    // Asociar la función de inicio de sesión al botón
-    document.querySelector('button').addEventListener('click', function() {
+    // Asociar la función de inicio de sesión al formulario
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        // Evitar el envío del formulario por defecto
+        event.preventDefault();
+
         // Obtener los valores de los campos de usuario y contraseña
         var usuario = document.querySelector('input[type="email"]').value;
         var contrasena = document.querySelector('input[type="password"]').value;
