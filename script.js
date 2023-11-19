070d9baae53f930a18151d9c786f0db7fa5bea42
Paul-Arteaga <150304081+Paul-Arteaga@users.noreply.github.com>
2023-11-19 18:38:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+function toggleOpciones() {
+    var opciones = document.getElementById("opciones");
+    if (opciones.style.display === "block") {
+        opciones.style.display = "none";
+    } else {
+        opciones.style.display = "block";
+    }
+}
+
+function irAMenuPrincipal() {
+    alert("Ir a Menú Principal");
+    // Puedes agregar aquí la lógica para redirigir a la página del menú principal
+}
+
+function irAConfigurar() {
+    alert("Ir a Configurar");
+    // Puedes agregar aquí la lógica para redirigir a la página de configuración
+}
+
+function salir() {
+    alert("Salir");
+    // Puedes agregar aquí la lógica para salir o cerrar la aplicación
+}
+function irAMenuPrincipal() {
+    window.location.href = "login.html";
+}
+function salir() {
+    window.location.href = "index.html";
+}
+// script.js
+
+function agregarAlCarrito(nombre, precio) {
+    var carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    carrito.push({ nombre: nombre, precio: precio });
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+  
+    alert('Producto agregado al carrito');
+  }
+
+
+
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
