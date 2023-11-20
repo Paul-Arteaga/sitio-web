af65cc2cddd88ba8b57e43418e91b63dad5cc191
ernesto arteaga <paul>
2023-11-20 16:36:27 -0400

    Updates

--- script.js
+++ script.js
@@ -29,50 +29,56 @@ function salir() {
 }
 // script.js
 
-function agregarAlCarrito(nombre, precio) {
-    var carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-    carrito.push({ nombre: nombre, precio: precio });
-    localStorage.setItem('carrito', JSON.stringify(carrito));
-  
-    alert('Producto agregado al carrito');
-  }
+// script.js
 
+let slides = document.querySelectorAll(".slide-ana>div");
+let slideSayisi = slides.length;
+let loop = 0 + 1000 * slideSayisi;
 
-/* // script.js
-
-let map;
-
-function initMap() {
-  try {
-    const position = { lat: -17.802631378173828, lng: -63.171173095703125 };
-
-    // Esperar a que la API de Google Maps se cargue completamente
-    google.maps.event.addDomListenerOnce(window, 'load', function () {
-      // Verificar si la librería de Google Maps está disponible
-      if (typeof google.maps === 'object') {
-        const { Map, Marker } = google.maps;
-
-        map = new Map(document.getElementById("map"), {
-          zoom: 14,
-          center: position,
-          mapId: "DEMO_MAP_ID",
-        });
-
-        const marker = new Marker({
-          map: map,
-          position: position,
-          title: "My location",
-        });
-      } else {
-        console.error('Error: La librería de Google Maps no está disponible.');
-      }
-    });
-  } catch (error) {
-    console.error('Error al inicializar el mapa:', error);
+function goNext() {
+  loop++;
+  for (let index = 0; index < slides.length; index++) {
+    const element = slides[index];
+    element.style.transform =
+      "translateX(" + 100 * (index - (loop % slideSayisi)) + "%)";
   }
 }
- */
-// script.js
+
+function goPrev() {
+  loop--;
+  for (let index = 0; index < slides.length; index++) {
+    const element = slides[index];
+    element.style.transform =
+      "translateX(" + 100 * (index - (loop % slideSayisi)) + "%)";
+  }
+}
+
+function openView(nombre, precio) {
+  document.getElementById("viewerButton").classList.add("hidden");
+  document.getElementById("viewerBox").classList.remove("hidden");
+
+  // Actualizar información del producto en la ventana emergente
+  document.getElementById("productTitle").innerText = nombre;
+  document.getElementById("productPrice").innerText = `$${precio}`;
+}
+
+function closeView() {
+  document.getElementById("viewerBox").classList.add("hidden");
+  document.getElementById("viewerButton").classList.remove("hidden");
+}
+
+function agregarAlCarrito(nombre, precio) {
+  var carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+  carrito.push({ nombre: nombre, precio: precio });
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+
+  // Mostrar la ventana emergente al añadir al carrito
+  openView(nombre, precio);
+}
+
+// Resto del código...
+
+
 
 let map;
 
